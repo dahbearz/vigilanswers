6e83f7bb1166eea87f99f159d0e4f246416bf5c1
MRider <bisham3@gatech.edu>
2012-12-01 15:01:23 -0500

    Fixed categories and reports associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ActiveRecord::Base
+  has_many :reports
   attr_accessible :name
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121201193947) do
+ActiveRecord::Schema.define(:version => 20121201195952) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
-
 
   create_table "locations", :force => true do |t|
     t.integer "latitude",  :limit => 8
@@ -34,6 +33,7 @@ ActiveRecord::Schema.define(:version => 20121201193947) do
     t.integer "longitude"
     t.integer "latitude"
     t.integer "report_hour_age"
+    t.integer "category_id"
   end
 
   create_table "sessions", :force => true do |t|
