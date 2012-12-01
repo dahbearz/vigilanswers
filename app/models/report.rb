49758c170a075178e56fd11102a2b81d0df98545
Peter <parkper@gmail.com>
2012-12-01 18:08:40 -0500

    FUCK"

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,18 +2,20 @@ class Report < Authlogic::Session::Base
   belongs_to :category
 
   def calculate_score
-    return (@vote - 1) / (@report_hour_age + 2)**(1.8)
+    return (@vote - 1) / (refresh_hour_age + 2)**(1.8)
   end
 
   def most_relavant(params)
     list = scoped
     list = list.where("title like ?", params[:title])
-    list = list.where("location_id IN (:location)", :location => Location.near([params[:lat],params[:lon]],params[:range],:order => :distance))
+    list = list.where("location_id IN (:location)", :location => Location.near([params[:lat],params[:lon]],params[:range]))
 
-    list.each do |report|
-      report.calculate_score
-    end
+    list.sort_by { |a,b| a.calculate_score <=> b.calculate_score}
 
     return list
+  end
+
+  def refresh_hour_age
+    @report_hour_age =  (self.updated_at - self.created_at) * 3600
   end
 end
