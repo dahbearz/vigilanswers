cf7cfc0581b8461bf11bdab2f6c567b0aaeaa1ec
Peter <parkper@gmail.com>
2012-12-01 12:13:57 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crimemopper::Application.config.secret_token = '791a0fa67b52945156436c875dbb23a70a18b192ee90d877bc33e3f34afd7754bafef63ca19614d4b7f2afe383591f157c1bcc71db880493c7fb44674d5afd03'
