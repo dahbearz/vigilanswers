48c53a0f5b56e8b7052335db8307d5288ac8a1f4
MRider <bisham3@gatech.edu>
2012-12-01 16:29:01 -0500

    Created location/report model associations and started report creation template

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,5 +1,6 @@
 class Report < Authlogic::Session::Base
   belongs_to :category
+  has_one :location
   
   def calculate_score(votes, item_hour_age)
     return (votes - 1) / (item_hour_age + 2)**(1.8)
