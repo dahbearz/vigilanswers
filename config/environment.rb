63bf8f395f07582af7d22b0b5015ab38f25ff2ed
jakswa <jakswa@gmail.com>
2012-12-02 12:04:51 -0500

    ActiveSupport - YOU CRAZY MONKEY.  NO time!

--- config/environment.rb
+++ config/environment.rb
@@ -5,14 +5,16 @@ require File.expand_path('../application', __FILE__)
 Vigilanswers::Application.initialize!
 
 #Mailer Stuffs (SendGrid)
-config.action_mailer.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-  :user_name => "groopfly",
-  :password => "baconbacon",
-  :domain => "lit-ridge-7864.herokuapp.com",
-  :address => "smtp.sendgrid.net",
-  :port => 25,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
-config.action_mailer.raise_delivery_errors = true
+Vigilanswers::Application.configure do 
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :user_name => "groopfly",
+    :password => "baconbacon",
+    :domain => "lit-ridge-7864.herokuapp.com",
+    :address => "smtp.sendgrid.net",
+    :port => 25,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+  config.action_mailer.raise_delivery_errors = true
+end
